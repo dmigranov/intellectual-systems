aa58c7a4b843db0eae0379ad964c29c8a561c1bd
dmigranov <d.migranov@g.nsu.ru>
2021-11-26 11:14:18 +0700

    some

---
+++ author_recognition.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numpy import log
+
+import nltk
+from nltk import word_tokenize
+from nltk import ngrams
+
+import string
+
+def log0(x):
+    return 0 if x <= 0 else log(x)
+
+    
