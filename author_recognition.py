055833e19d32fb52b46b7d8b25d346299a142ccc
dmigranov <d.migranov@g.nsu.ru>
2021-11-26 11:21:53 +0700

    getTransitionMatrixFromFile

--- author_recognition.py
+++ author_recognition.py
@@ -10,9 +10,9 @@ import string
 def log0(x):
     return 0 if x <= 0 else log(x)
 
-def getTMatrixFromFile(filename):
+def getTransitionMatrixFromFile(filename):
     with open(filename, 'r', encoding = 'utf-8') as file:
         text = word_tokenize(file.read().replace('\n', ' '))
         cleaned_text = getRidOfPunctuationAndUpperWords(text)
 
-        return computeTMatrix(cleaned_text)
+        return computeTransitionMatrix(cleaned_text)
