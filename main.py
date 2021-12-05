60f8958e93b7121a77278a4fe4cdf6a9079a80d7
dmigranov <d.migranov@g.nsu.ru>
2021-12-05 15:04:54 +0700

    not bad

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from author_recognition import *
 
 classifier = AuthorClassifier()
 classifier.train([["Text/Tolstoy_train"], ["Text/Dostoevsky_train"], ], ["Толстой", "Достоевский",])
-print(classifier.predict(["Text/Dostoevsky_test1", "Text/Tolstoy_test1", "Text/Tolstoy_test2"]))
+print(classifier.predict(["Text/Dostoevsky_test1", "Text/Tolstoy_test1", "Text/Tolstoy_test2", "Text/Tolstoy_test3"]))
