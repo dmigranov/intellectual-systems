c159fe2a74153f8f3356e07ef38adbcb9b485332
dmigranov <d.migranov@g.nsu.ru>
2021-12-05 17:26:34 +0700

    ALRIGHT

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from author_recognition import *
 
 classifier = AuthorClassifier()
 classifier.train([["Text/Tolstoy_train"], ["Text/Dostoevsky_train"], ["Text/Turgenev_train"], ["Text/Gogol_train"], ["Text/Strug_train"], ["Text/Bulych_train"]], ["Толстой", "Достоевский", "Тургенев", "Гоголь", "Стругацкие", "Булычёв"])
-print(classifier.predict(["Text/Dostoevsky_test1", "Text/Dostoevsky_test2", "Text/Tolstoy_test1", "Text/Tolstoy_test2", "Text/Tolstoy_test3", "Text/Turgenev_test1", "Text/Turgenev_test2", "Text/Gogol_test1", "Text/Gogol_test2", "Text/Gogol_test3", "Text/Strug_test1", "Text/Strug_test2", "Text/Bulych_test1",]))
+print(classifier.predict([  "Text/Dostoevsky_test1", "Text/Dostoevsky_test2", "Text/Tolstoy_test1", "Text/Tolstoy_test2", "Text/Tolstoy_test3", 
+                            "Text/Turgenev_test1", "Text/Turgenev_test2", "Text/Gogol_test1", "Text/Gogol_test2", "Text/Gogol_test3", 
+                            "Text/Strug_test1", "Text/Strug_test2", "Text/Bulych_test1", "Text/Bulych_test2", "Text/Bulych_test3",]))
