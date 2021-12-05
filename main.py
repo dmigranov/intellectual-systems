7bf7d4454ce1ac4d7e26a08934061a2d1de9ff63
dmigranov <d.migranov@g.nsu.ru>
2021-12-05 16:39:07 +0700

    not bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from author_recognition import *
 
 classifier = AuthorClassifier()
-classifier.train([["Text/Tolstoy_train"], ["Text/Dostoevsky_train"], ["Text/Turgenev_train"], ["Text/Bulgakov_train"], ["Text/Gogol_train"]], ["Толстой", "Достоевский", "Тургенев", "Булгаков", "Гоголь"])
-print(classifier.predict(["Text/Dostoevsky_test1", "Text/Dostoevsky_test2", "Text/Tolstoy_test1", "Text/Tolstoy_test2", "Text/Tolstoy_test3", "Text/Turgenev_test1", "Text/Turgenev_test2", "Text/Bulgakov_test1", "Text/Gogol_test1", "Text/Gogol_test2", "Text/Gogol_test3",]))
+classifier.train([["Text/Tolstoy_train"], ["Text/Dostoevsky_train"], ["Text/Turgenev_train"], ["Text/Gogol_train"], ["Text/Strug_train"]], ["Толстой", "Достоевский", "Тургенев", "Гоголь", "Стругацкие"])
+print(classifier.predict(["Text/Dostoevsky_test1", "Text/Dostoevsky_test2", "Text/Tolstoy_test1", "Text/Tolstoy_test2", "Text/Tolstoy_test3", "Text/Turgenev_test1", "Text/Turgenev_test2", "Text/Gogol_test1", "Text/Gogol_test2", "Text/Gogol_test3", "Text/Strug_test1", "Text/Strug_test2",]))
